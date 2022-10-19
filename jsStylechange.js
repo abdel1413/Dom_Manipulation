b27d21d0bc66dc132a9ea0680b71b90e3d1ea965
Your Name <you@example.com>
2022-10-19 10:43:26 -0400

    styling from js

--- jsStylechange.js
+++ jsStylechange.js
@@ -6,12 +6,16 @@ para.style.color = "green";
 let btn = document.getElementsByTagName("button")[0];
 
 btn.addEventListener("click", function () {
-  console.log("llisstenddd");
   let none = document.getElementById("none");
-
   if (none.style.display === "none") {
     none.style.display = "block";
+    none.style["font-style"] = "italic";
+    none.style["font-weight"] = "larger";
   } else {
     none.style.display = "none";
   }
 });
+
+//note: for the syle properties which have hiphen btw them, we
+// access them using like the way we access arrays using index
+// but here we pass a string not a number
